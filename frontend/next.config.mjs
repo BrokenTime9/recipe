1c4bca14c540e35c0414a72b4a2d0ece7f8361cc
BrokenTime9 <cerberuscraniax@gmail.com>
2025-05-11 13:09:46 +0530

    next js degrade

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false, // ⛔ disables SWC minifier
+	swcMinify: false, // ⛔ disables SWC minifier
 };
 
 export default nextConfig;
